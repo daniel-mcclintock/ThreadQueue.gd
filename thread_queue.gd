0647bd592842372021b8410219a66121e0076ba6
daniel-mcclintock <13973653+daniel-mcclintock@users.noreply.github.com>
2021-10-01 16:15:16 +1000

    Update Thread usage to current Godot-master

--- thread_queue.gd
+++ thread_queue.gd
@@ -1,15 +1,15 @@
 class_name ThreadQueue
-extends Reference
+extends RefCounted
 # Thread helper for batch jobs.
 
 signal _thread_finished
 
-var threads_max : int = 1
+var threads_max: int = 1
 
-var _threads : int = 0
-var _jobs : Array = Array()
-var _mtx : Mutex = Mutex.new()
-var _working : bool = false
+var _threads: int = 0
+var _jobs := []
+var _mtx := Mutex.new()
+var _working: bool = false
 
 func _nop(arg=null): pass
 
@@ -20,22 +20,30 @@ func _init(tmax : int = 1):
     threads_max = tmax
     _thread_finished.connect(__thread_finished)
 
+func clear_jobs() -> void:
+    _mtx.lock()
+    _jobs.clear()
+    _mtx.unlock()
+
 func add_job(function: Callable, callback: Callable = _nop, arguments = null) -> void:
     _mtx.lock()
     _jobs.append(Job.new(function, callback, arguments))
     _mtx.unlock()
 
+
 func start() -> void:
+    _mtx.lock()
+
     if not _working:
         _working = true
 
         while _working and _jobs.size() and _threads < threads_max:
-            _mtx.lock()
+
             var thread = Thread.new()
             _threads += 1
 
-            thread.start(self, "_work", thread, Thread.PRIORITY_LOW)
-            _mtx.unlock()
+            thread.start(_work, thread, Thread.PRIORITY_LOW)
+    _mtx.unlock()
 
 func _work(thread) -> void:
     while true:
@@ -51,12 +59,19 @@ func _work(thread) -> void:
             _mtx.unlock()
             break
 
-    _thread_finished.emit(thread)
+    # Shitty hack so that we safely(i think) end this Thread
+    (
+        func(_thread_finished, thread):
+            _thread_finished.emit(thread)
+    ).call_deferred(_thread_finished, thread)
 
 func __thread_finished(thread: Thread) -> void:
     _mtx.lock()
-    #thread.wait_to_finish() # thread is already dead
     _threads -= 1
+
+    # Fairly certain that this Thread should already be dead
+    #if thread.is_active():
+    #    thread.wait_to_finish.call_deferred()
 
     if _threads < 0:
         _threads = 0
@@ -66,7 +81,7 @@ func __thread_finished(thread: Thread) -> void:
 
     _mtx.unlock()
 
-class Job extends Reference:
+class Job extends RefCounted:
     var _function : Callable
     var _callback : Callable
     var _arguments
